0623a7ab9100031705ba7809c7742de3fa672dee
Duke Nguyen <conan4787@gmail.com>
2024-04-21 17:05:33 -0600

    Adding Logger project for the Game.

---
+++ Logger/CustomFileLogger.cs
@@ -0,0 +1,88 @@
+﻿/// <summary>
+/// Date: 14-Apr-2024
+/// Course: CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500, Khoa Minh Ngo and Duke Nguyen - This work may not
+///            be copied for use in Academic Coursework.
+/// 
+/// We, Khoa Minh Ngo and Duke Nguyen, certify that we wrote this code from scratch and
+/// did not copy it in part or whole from another source. All references used in the completion 
+/// of the assignments are cited in my README file
+/// 
+/// File Contents
+/// 
+///     This library class represents a custom file logger.    
+/// </summary>
+
+using Microsoft.Extensions.Logging;
+
+namespace Logger
+{
+    /// <summary>
+    /// Represents a custom file logger implementation.
+    /// </summary>
+    public class CustomFileLogger : ILogger
+    {
+        public readonly string filename;
+
+        /// <summary>
+        /// Construct a Custom File Logger
+        /// </summary>
+        /// <param name="categoryName">The category name for the logger</param>
+        public CustomFileLogger(string categoryName)
+        {
+            filename = Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData)
+                + Path.DirectorySeparatorChar
+                + $"CS3500-{categoryName}.log";
+
+        }
+
+        /// <summary>
+        /// Creates a new scope for logging
+        /// </summary>
+        /// <typeparam name="TState">type of the scope state</typeparam>
+        /// <param name="state">the scope state</param>
+        /// <returns>An IDisposable that ends the logging scope when disposed.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if the given log level is enabled.
+        /// </summary>
+        /// <param name="logLevel">The log level to check</param>
+        /// <returns>True if the log level is enabled, otherwise false</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// write a log entry
+        /// </summary>
+        /// <typeparam name="TState">type of the log state</typeparam>
+        /// <param name="logLevel">the log level of the log entry</param>
+        /// <param name="eventId">the EventId of the log entry</param>
+        /// <param name="state">The state of the log entry</param>
+        /// <param name="exception">exception of log entry</param>
+        /// <param name="formatter">the function that creates log message</param>
+
+        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            string logMessage = $"{DateTimeOffset.Now:yyyy-MM-dd h:mm:ss tt} ({Thread.GetDomainID()}) - {logLevel} - This message was produced by a Log{logLevel} call. Content: {formatter(state, exception)}{Environment.NewLine}";
+
+            try
+            {
+                File.AppendAllText(filename, logMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Writing log file '{filename}' failed: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Logger/CustomFileLoggerProvider.cs
@@ -0,0 +1,68 @@
+﻿/// <summary>
+/// Date: 14-Apr-2024
+/// Course: CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500, Khoa Minh Ngo and Duke Nguyen - This work may not
+///            be copied for use in Academic Coursework.
+/// 
+/// We, Khoa Minh Ngo and Duke Nguyen, certify that we wrote this code from scratch and
+/// did not copy it in part or whole from another source. All references used in the completion 
+/// of the assignments are cited in my README file
+/// 
+/// File Contents
+///     
+///     This class represents a custom logger provider that creates logger instances for logging to files.
+///     
+/// </summary>
+
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+
+namespace Logger
+{
+    /// <summary>
+    /// A custom logger provider that creates logger instances for logging to files   
+    /// </summary>
+    public class CustomFileLoggerProvider : ILoggerProvider
+    {
+        // Store created logger instances
+        private readonly ConcurrentDictionary<string, ILogger> _loggers = new ConcurrentDictionary<string, ILogger>();
+
+        // minimum log level to be written
+        private LogLevel _logLevel = LogLevel.Information;
+
+        /// <summary>
+        /// State of the logger provider
+        /// </summary>
+        public object State
+        {
+            get => _logLevel;
+            set => _logLevel = (LogLevel)value;
+        }
+
+        /// <summary>
+        /// Create logger instance given an inputted category name
+        /// </summary>
+        /// <param name="categoryName">category of the logger</param>
+        /// <returns>logger instance</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new CustomFileLogger(categoryName);
+        }
+
+        /// <summary>
+        /// Dispose created logger instances
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Dispose()
+        {
+            foreach (var logger in _loggers.Values)
+            {
+                (logger as IDisposable)?.Dispose();
+            }
+
+            _loggers.Clear();
+        }
+    }
+}
