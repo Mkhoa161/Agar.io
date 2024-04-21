dc6fdc00867b15a368e39d02e4580d1a5e24bbfa
Duke Nguyen <conan4787@gmail.com>
2024-04-21 17:10:42 -0600

    Fixing the MAUI project to use Logger in MainPage.

--- ClientGUI/MauiProgram.cs
+++ ClientGUI/MauiProgram.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.Extensions.Logging;
+﻿/// <summary>
+/// Date: 14-Apr-2024
+/// Course: CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500, Khoa Minh Ngo and Duke Nguyen - This work may not
+///            be copied for use in Academic Coursework.
+/// 
+/// We, Khoa Minh Ngo and Duke Nguyen, certify that we wrote this code from scratch and
+/// did not copy it in part or whole from another source. All references used in the completion 
+/// of the assignments are cited in my README file
+/// 
+/// File Contents
+/// 
+///     This class provides a static method for creating a MauiApp instance.
+/// </summary>
+
+using Logger;
+using Microsoft.Extensions.Logging;
 
 namespace ClientGUI
 {
+    /// <summary>
+    /// Provides a static method for creating a MauiApp instance.
+    /// </summary>
     public static class MauiProgram
     {
+        /// <summary>
+        /// Creates and configures a MauiApp instance.
+        /// </summary>
+        /// <returns>The configured MauiApp instance.</returns>
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -13,11 +36,16 @@ namespace ClientGUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+                .Services.AddLogging(configure =>
+                {
+                    configure.AddDebug();
+                    configure.AddProvider(new CustomFileLoggerProvider());
+                    configure.SetMinimumLevel(LogLevel.Debug);
+                })
+                .AddTransient<MainPage>();
+
 
             return builder.Build();
         }
